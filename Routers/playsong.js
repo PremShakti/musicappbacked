22d6ccf0e6823ff00aebcb28938c67f11b1c6171
PremShakti <premshakti.ps@gmail.com>
2023-06-25 11:15:48 +0530

    a

--- Routers/playsong.js
+++ Routers/playsong.js
@@ -4,36 +4,99 @@ const Routerplay=express.Router()
 let usetube=require('usetube')
 const ytdl = require('ytdl-core');
 
-const ffmpeg = require('fluent-ffmpeg');
-
-
-Routerplay.use('/play', (req, res, next) => {
-  console.log(req.query.url)
-    res.setHeader('Content-Disposition', 'attachment; filename="audio.mp3"');
-    res.setHeader('Content-Type', 'audio/mpeg');
-    next();
-  });
 
 
 
-Routerplay.get('/play', (req, res) => {
+// Routerplay.use('/play', (req, res, next) => {
+//   console.log(req.query.url)
+//     res.setHeader('Content-Disposition', 'attachment; filename="audio.mp3"');
+//     res.setHeader('Content-Type', 'audio/mpeg');
+//     next();
+//   });
+
+
+
+Routerplay.get('/play', async(req, res) => {
     const videoUrl = req.query.url; // Get the video URL from the query string
   
-    // Set the response headers to stream the converted MP3 file
-//     res.header('Content-Disposition', 'attachment; filename="audio.mp3"');
-//     res.header('Content-Type', 'audio/mpeg');
-//       const audioStream = ytdl(videoUrl, { quality: 'highestaudio' });
-//   audioStream.pipe(res)
+
 
   try {
-    const audioStream = ytdl(videoUrl, { quality: 'highestaudio' });
-  
-    audioStream.on('error', (err) => {
-      console.error('Error during audio streaming:', err);
-      res.status(500).send('Error streaming audio');
-    });
-  
-    audioStream.pipe(res);
+    res.setHeader('Content-Type', 'audio/mpeg');
+
+
+
+    
+
+    // Get video details including the thumbnail URL
+    const videoInfo = await ytdl.getInfo(videoUrl);
+
+    // Extract the thumbnail URL
+    const thumbnailUrl = videoInfo.videoDetails.thumbnail.thumbnails[0].url;
+
+    // Set the response header for the thumbnail URL
+    res.setHeader('Access-Control-Expose-Headers', 'X-Thumbnail-URL');
+    res.setHeader('X-Thumbnail-URL', thumbnailUrl);
+
+    // Pipe the audio stream directly to the response
+    ytdl(videoUrl, {
+      filter: 'audioonly',
+      quality: 'highestaudio',
+      format: 'mp3',
+    }).pipe(res);
+
+
+    // Get the audio stream
+    // const audioStream = ytdl(videoUrl, {
+    //   filter: 'audioonly',
+    //   quality: 'highestaudio',
+    //   format: 'mp3',
+    // });
+
+    // Calculate the total size of the audio file in bytes
+    // const audioSize = await ytdl.getBasicInfo(videoUrl)
+    //   .then(info => info.videoDetails.lengthSeconds * info.formats[0].bitrate / 8);
+
+    // // Set the Content-Length header with the audio size
+    // res.setHeader('Content-Length', audioSize);
+
+    // // Pipe the audio stream directly to the response with a fixed buffer size
+    // audioStream.pipe(res, { end: true, bufferSize: 64 * 1024 });
+
+
+
+    // const videoInfo = await ytdl.getInfo(videoUrl);
+
+    // // Extract the thumbnail URL
+    // const thumbnailUrl = videoInfo.videoDetails.thumbnail.thumbnails[0].url;
+
+    // res.setHeader('Content-Type', 'audio/mpeg');
+    // res.setHeader('Transfer-Encoding', 'chunked');
+
+    // // Pipe the audio stream directly to the response
+    // ytdl(videoUrl, {
+    //   filter: 'audioonly',
+    //   quality: 'highestaudio',
+    //   format: 'mp3',
+    // }).pipe(res);
+    // res.setHeader('X-Thumbnail-URL', thumbnailUrl);
+
+   
+  //   const audioStream = ytdl(videoUrl, { quality: 'highestaudio' });
+  // console.log(audioStream)
+  //   audioStream.on('error', (err) => {
+  //     console.error('Error during audio streaming:', err);
+  //     res.status(500).send('Error streaming audio');
+  //   });
+    
+    
+
+  //   audioStream.pipe(res);
+
+
+
+
+
   } catch (err) {
     console.error('Error retrieving video:', err);
     res.status(500).send('Error retrieving video');
@@ -79,8 +142,11 @@ Routerplay.get('/search', async(req, res) => {
 
 try {
 if(search){
+  
 const data=await usetube.searchVideo(search)
-
+// const videoInfo = await usetube.getVideoInfo(videoUrl);
+//     const thumbnailUrl = videoInfo.thumbnailUrl;
+// console.log(thumbnailUrl)
 // res.status(200)._construct({url:`https://www.youtube.com/watch?v=DP2T2W1IjA4`})
 res.status(200).json({data:data})
 }
--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const { Routerplay } = require('./Routers/playsong');
 const app = express();
 require('dotenv').config()
 const cors=require('cors')
+app.use(cors())
 let PORT=process.env.PORT
 app.use(express.json())
-app.use(cors())
 app.use('/',Routerplay)
 
 
 
 
 // Start the server
-app.listen(PORT||8080, () => {
+app.listen(8080, () => {
 try {
-    console.log(`server is running on port ${PORT}`);
+    console.log(`server is running on port 8080`);
 } catch (error) {
     console.log(error)
 }
